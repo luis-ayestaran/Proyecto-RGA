78bc835fc9a4544eb5187f998ec50a5775e56ccf
luis-ayestaran <luisfer_ayes98@hotmail.com>
2020-11-29 19:44:27 -0600

    Se crea la clase buap-tec_ia.proyecto.estructuras.Grafo para almacenar la información y los métodos de inicialización de los costos del grafo

---
+++ Proyecto-RGA/src/main/java/buap/tec_ia/proyecto/estructuras/Grafo.java
@@ -0,0 +1,31 @@
+package buap.tec_ia.proyecto.estructuras;
+
+public class Grafo {
+	
+	private int[][] costos;
+	
+	public Grafo() {
+		
+		init();
+		
+	}
+	
+	public void init() {
+		
+		leeDatos();
+		//aleatoriamente();
+		//fijo();
+		
+		
+	}
+	
+	private void leeDatos() {
+		
+		//System.out.println("Propor");
+		//int vertices = //;
+		//costos = new int[vertices][vertices];
+		
+	}
+	
+
+}
