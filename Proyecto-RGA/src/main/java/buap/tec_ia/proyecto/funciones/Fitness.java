37bda370e5d42b6cb829bad5e531033bfb1b8848
Josue <j.pll@hotmail.com>
2020-11-30 16:47:52 -0600

    Beta 1 func fitness, genera un error por el arreglo y cree la clase
    fitnesstest para probarla

--- Proyecto-RGA/src/main/java/buap/tec_ia/proyecto/funciones/Fitness.java
+++ Proyecto-RGA/src/main/java/buap/tec_ia/proyecto/funciones/Fitness.java
@@ -1,7 +1,23 @@
 package buap.tec_ia.proyecto.funciones;
 
+import buap.tec_ia.proyecto.estructuras.Individuo;
+
 public class Fitness {
+	private Individuo individuo;
 	
 	
+	public Fitness() {
+		init();
+	}
+	
+	public void init() {
+		Funcion_Fitness(individuo);
+	}
+	
+	public void Funcion_Fitness(Individuo individuo) {
+		
+		System.out.println(individuo.getSv());
+		
+	}
 
 }
---
+++ Proyecto-RGA/src/test/java/buap/tec_ia/proyecto/funciones/FitnessTest.java
@@ -0,0 +1,13 @@
+package buap.tec_ia.proyecto.funciones;
+
+import org.junit.Test;
+
+public class FitnessTest {
+	
+	@Test
+    public void test() {
+    	Fitness fitness = new Fitness();
+    }
+}
+
+
