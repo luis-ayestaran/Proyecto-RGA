499f4d012600814d5de57943492c04d49cbc0a88
Josue <j.pll@hotmail.com>
2020-11-30 20:03:39 -0600

    beta 2 func fitness

--- Proyecto-RGA/src/main/java/buap/tec_ia/proyecto/funciones/Fitness.java
+++ Proyecto-RGA/src/main/java/buap/tec_ia/proyecto/funciones/Fitness.java
@@ -3,21 +3,11 @@ package buap.tec_ia.proyecto.funciones;
 import buap.tec_ia.proyecto.estructuras.Individuo;
 
 public class Fitness {
-	private Individuo individuo;
 	
-	
-	public Fitness() {
-		init();
-	}
-	
-	public void init() {
-		Funcion_Fitness(individuo);
-	}
-	
-	public void Funcion_Fitness(Individuo individuo) {
+	public static void funcionFitness(Individuo individuo) {
 		
-		System.out.println(individuo.getSv());
+		System.out.print(individuo.getRecorrido());
 		
-	}
+    }
 
 }
--- Proyecto-RGA/src/test/java/buap/tec_ia/proyecto/funciones/FitnessTest.java
+++ Proyecto-RGA/src/test/java/buap/tec_ia/proyecto/funciones/FitnessTest.java
@@ -2,11 +2,14 @@ package buap.tec_ia.proyecto.funciones;
 
 import org.junit.Test;
 
+import buap.tec_ia.proyecto.estructuras.Individuo;
+
 public class FitnessTest {
 	
 	@Test
     public void test() {
-    	Fitness fitness = new Fitness();
+    	Individuo individuo = new Individuo(3);
+    	
     }
 }
 
