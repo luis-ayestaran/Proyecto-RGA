573fd26e9777f52f5e17b294ffe847f7ab9690ae
Josue <j.pll@hotmail.com>
2020-12-01 20:44:30 -0600

    Se modifico la validacion del rango y de nodo repetido, ya
    no genera error y si detecta los errores en fitness.java

--- Proyecto-RGA/src/main/java/buap/tec_ia/proyecto/funciones/Fitness.java
+++ Proyecto-RGA/src/main/java/buap/tec_ia/proyecto/funciones/Fitness.java
@@ -13,55 +13,54 @@ public class Fitness {
 	    double costoTotal=0;
 	    int bandera=0;
 	    int valor;
-	    boolean repetido=false;
+	    int repetido=0;
 	    boolean camino=false;
-	    
+	    boolean band=false;
+	   
 	  //se valida que el vertice este dentro del rango
 		for(int i=0;i<individuo.getRecorrido().length;i++) {
 			System.out.print(individuo.getRecorrido()[i] +"\t"); //nodos que se visitaron del grafo
-			
-			if(individuo.getRecorrido()[i]<0 && individuo.getRecorrido()[i]>=individuo.getRecorrido().length) {
+			if(individuo.getRecorrido()[i]<0 || individuo.getRecorrido()[i]>=individuo.getRecorrido().length) {
 				System.out.println("Nodo fuera del rango\n");
 				individuo.setAptitud((float) (1.0/inf));
+				System.out.println("Aptitud:"+individuo.getAptitud());
+				band=true;
 				break;
 			}	
 		}
 		System.out.println("\n");
 		
 		//se valida que el vertice no este repetido
-		
 		valor=individuo.getRecorrido()[0];
-		
 		do {
 			
 			for(int i=0;i<individuo.getRecorrido().length;i++) {
 				//System.out.print(individuo.getRecorrido()[i] +"\t"); 
 				if(individuo.getRecorrido()[i]==valor) {
-					repetido=true;
-				}else {
-					repetido=false;
+					repetido=repetido+1;
 				}
 			}
 			bandera=bandera+1;
 			valor=individuo.getRecorrido()[bandera];
-			
-			
+			if(repetido>=2){
+				System.out.println("Nodo repetido \n");
+				individuo.setAptitud((float) (1.0/inf));
+				System.out.println("Aptitud:"+individuo.getAptitud());
+				band=true;
+				break;
+			}
+			repetido=0;
 		}while(bandera!=(individuo.getRecorrido().length-1));
-		
-		if(repetido){
-			System.out.println("Nodo repetido\n");
-			individuo.setAptitud((float) (1.0/inf));
-		}
-		
 		System.out.println("\n");
 		
+		//validacion, camino existente, que no exista -1 en los costos
 		
+		if(band==false){
 		int[] costoAristas = grafo.calcularCostoAristas(individuo.getRecorrido());
 		System.out.println("Costos:\n");
 		for(int i=0;i<costoAristas.length;i++) {
 			
 			System.out.print(costoAristas[i] +"\t");
-			//primera validacion, camino existente, que no exista -1 en los costos
 			if(costoAristas[i]!=-1)
 			{
 				costoTotal=costoAristas[i]+costoTotal;
@@ -81,8 +80,8 @@ public class Fitness {
 			System.out.println("\n");
 			individuo.setAptitud((float) (1.0/costoTotal));
 			System.out.println("Aptitud:"+individuo.getAptitud());
-		}
-
+		  }
+	   }
 
      }
 
